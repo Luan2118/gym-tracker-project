dbb2e52e1676c8fd5f1bf1bdd0cb517df4f42d22
Luan <leluanvn21@seznam.cz>
2026-01-17 13:22:29 +0100

    feat: add start workout form layout

--- src/pages/ActiveWorkout/ActiveWorkout.jsx
+++ src/pages/ActiveWorkout/ActiveWorkout.jsx
@@ -1,6 +1,9 @@
 import styles from  './ActiveWorkout.module.css'
 
 export default function ActiveWorkout() {
+
+
+
   return (
     <>
     <header>
@@ -10,13 +13,22 @@ export default function ActiveWorkout() {
 
     <div className={styles["content-wrapper"]}>
 
-      <div className={styles["start-workout-button-wrapper"]}>
-        <button className={styles["start-workout-button"]}>
-          Start  Workout
+      <div className={styles["start-workout-wrapper"]}>
+        <button type='button' className={styles["start-workout-button"]}>
+          Start a Workout
         </button>
+
+        <form className={styles["form-wrapper"]}>
+          <label htmlFor="training-split" className={styles["training-split-text"]}>Training Split: </label>
+          <select id="training-split" className={styles["select-input"]}>
+            <option  id="" selected disabled>Select a Split</option>
+            <option value="Upper/Lower"  className={styles["select-option"]}>Upper / Lower</option>
+            <option value="PPL">PPL</option>
+            <option value="Fullbody">Fullbody</option>
+          </select>
+        </form>
       </div>
 
-      
       <section className={styles["content-main"]}>
         <h2>No Active Workout</h2>
       </section>
