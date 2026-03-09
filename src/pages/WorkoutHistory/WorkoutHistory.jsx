f0c481aa90c312fea23bd08c9fcf4768198c23bd
Luan <leluanvn21@seznam.cz>
2026-03-09 18:36:02 +0100

    feat: add Workout History page layout

--- src/pages/WorkoutHistory/WorkoutHistory.jsx
+++ src/pages/WorkoutHistory/WorkoutHistory.jsx
@@ -1,5 +1,50 @@
+import styles from './WorkoutHistory.module.css'
+
 export default function WorkoutHistory() {
   return (
-    <div>Workout History Page</div>
+    <>
+      <header>
+        <h1>Workout History</h1>
+      </header>
+
+      <div className={styles["content-wrapper"]}>
+        <section className={styles["section-wrapper"]}>
+          <h2>Last Workout</h2>
+
+
+        </section>
+
+        <section className={styles["section-wrapper"]}>
+          <h2>
+            All Workouts
+          </h2>
+
+          <section  className={styles["filter-wrapper"]}>
+            <h3  className={styles["sr-only"]}>Filter</h3>
+
+            <div>
+              <label htmlFor="search"></label>
+              <input className={styles["search-input"]} id="search" placeholder='Search workout' type='text'></input>
+
+              <label htmlFor="training-split" className={styles["sr-only"]}>Training Split</label>
+              <select id="training-split" className={styles["filter-input"]}>
+                <option value="Training Split">Training Split</option>
+              </select>
+
+               <label htmlFor="workout-day" className={styles["sr-only"]}>Workout Day</label>
+              <select id="workout-day" className={styles["filter-input"]}>
+                <option value="Workout Day">Workout Day</option>
+              </select>
+
+              <label htmlFor="sort" className={styles["sr-only"]}>Sort</label>
+              <select id="sort" className={styles["filter-input"]}>
+                <option value="Sort">Sort</option>
+              </select>
+
+            </div>
+          </section>
+        </section>
+      </div>
+    </>
   )
 }
