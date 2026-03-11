ce9d323e437275d129bf93ada033d8386aeb60ed
Luan <leluanvn21@seznam.cz>
2026-03-11 18:52:58 +0100

    refactor: change Exercises  page layout from vertical to horizontal

--- src/pages/Exercises/Exercises.jsx
+++ src/pages/Exercises/Exercises.jsx
@@ -7,47 +7,51 @@ export default function Exercises() {
   return (
     <>
       <header>
-        <h1>All Exercises</h1>
+        <h1>Exercise</h1>
       </header>
 
       <div className={styles["content-wrapper"]}>
-        <section className={styles["filter-wrapper"]}>
-          <h2 className={styles["filter-text"]}>Filter</h2>
 
-          <div className={styles["filter-input-wrapper"]}>
+        <div className={styles["main-content-wrapper"]}>
+          MAIN CONTENT
+        </div>
 
-            <div className={styles["filter-input-link"]}>
-              <label htmlFor='search-exercise' />
-              <input type="text" id="search-exercise" className={styles["search-exercise-input"]}/>
-              
-              <button className={styles["search-exercise-button"]}>Search</button>
+        <div>
+          <div className={styles["filter-wrapper"]}>
+            <div className={styles["filter-input-wrapper"]}>
+
+              <div className={styles["filter-input-link"]}>
+                <label htmlFor='search-exercise' />
+                <input type="text" id="search-exercise" className={styles["search-exercise-input"]} />
+
+                <button className={styles["search-exercise-button"]}>Search</button>
+              </div>
+
+              <div className={styles["filter-input-link"]}>
+
+                <label htmlFor="exercise-category" className={styles["by-muscle-category-label"]}>Category</label>
+                <select id="exercise-category" className={styles["by-muscle-category-input"]}></select>
+
+
+              </div>
+
+              <div className={styles["filter-input-link"]}>
+                <button className={styles["all-exercises"]}>All exercises</button>
+                <button className={styles["by-muscle-group-filter"]}>By Muscle Group</button>
+              </div>
+
+              <div className={styles["filter-input-link"]}>
+                <button className={styles["upper-body-exercises"]}>Upper Body Exercises</button>
+                <button className={styles["lower-body-exercises"]}>Lower Body Exercises</button>
+              </div>
+
             </div>
-
-            <div className={styles["filter-input-link"]}> 
-
-              <label htmlFor="exercise-category" className={styles["by-muscle-category-label"]}>Category</label>
-               <select id="exercise-category" className={styles["by-muscle-category-input"]}></select>  
-
-              
-            </div>
-
-            <div className={styles["filter-input-link"]}>
-              <button className={styles["all-exercises"]}>All exercises</button>
-              <button className={styles["by-muscle-group-filter"]}>By Muscle Group</button>
-            </div>
-
-            <div className={styles["filter-input-link"]}>
-              <button className={styles["upper-body-exercises"]}>Upper Body Exercises</button>
-              <button className={styles["lower-body-exercises"]}>Lower Body Exercises</button>
-            </div>
-
           </div>
-        </section>
 
-        <section aria-label='Exercise List' className={styles["content-main"]}>
-
-          <ExerciseList/>
-        </section>
+          <section aria-label='Exercise List' className={styles["exercise-list-wrapper"]}>
+            <ExerciseList />
+          </section>
+        </div>
       </div>
     </>
   )
