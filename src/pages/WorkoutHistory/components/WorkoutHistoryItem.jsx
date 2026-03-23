1480dfa5281b4e13ad65e89cdd6ae5b835a60998
Luan <leluanvn21@seznam.cz>
2026-03-23 17:22:08 +0100

    style: adjust workouthsitory item layout and displayed set

--- src/pages/WorkoutHistory/components/WorkoutHistoryItem.jsx
+++ src/pages/WorkoutHistory/components/WorkoutHistoryItem.jsx
@@ -2,6 +2,7 @@ import styles from './WorkoutHistoryItem.module.css'
 import arrowDown from '../../../assets/workout-history/arrow-down.png'
 import formatISODate from '../../../utils/formatISODate';
 import { useState } from 'react';
+import { EXERCISE_BASE_PREFIX } from '../../../data/exercises';
 import ExerciseSetsStat from '../../../components/Exercises/ExerciseSetsStat';
 
 
@@ -13,6 +14,7 @@ export default function WorkoutHistoryItem({ workoutHistory, filteredWorkoutHist
 
   // ex, exerciseId, workoutHistory, activeExIds, lastWorkout
 
+  console.log(selectedWorkoutHistItem)
 
   const activeExIds = new Set(selectedWorkoutHistItem?.exercises.map(e => e.exerciseId));
 
@@ -25,7 +27,7 @@ export default function WorkoutHistoryItem({ workoutHistory, filteredWorkoutHist
 
   function displaySelectedWorkoutHist(workoutId) {
     if (selectedWorkoutHisId === workoutId) setSelectedWorkoutHisId('')
-      else setSelectedWorkoutHisId(workoutId)
+    else setSelectedWorkoutHisId(workoutId)
   }
 
   return (
@@ -41,21 +43,40 @@ export default function WorkoutHistoryItem({ workoutHistory, filteredWorkoutHist
               <span className={styles["workout-history-item-day-text"]}>Date: {formatISODate(workout.date)}</span>
             </div>
 
-            <img src={arrowDown} alt="" className={workout.id === selectedWorkoutHisId ?styles["arrow-up-icon"] : styles["arrow-down-icon"]} />
+            <img src={arrowDown} alt="" className={workout.id === selectedWorkoutHisId ? styles["arrow-up-icon"] : styles["arrow-down-icon"]} />
           </button>
 
           {workout.id === selectedWorkoutHisId ?
             <div className={styles["selected-workout-history-wrapper"]}>
               {selectedWorkoutHistItem.exercises.map((ex) => {
                 return (
-                  <div key={ex.exerciseId} className={styles["selected-workout-history-exercise-wrapper"]}>
-                    <ExerciseSetsStat
-                      ex={ex}
-                      exerciseId={ex.exerciseId}
-                      workoutHistory={workoutHistory}
-                      activeExIds={activeExIds}
-                      lastWorkout={lastWorkout}
-                    />
+                  <div key={ex.exerciseId} className={styles["selected-workout-history-exercise-row"]}>
+                    <div className={styles["selected-workout-history-left"]}>
+                      <img
+                        src={`${EXERCISE_BASE_PREFIX}${ex.images[0]}`}
+                        alt=""
+                        className={styles["selected-workout-history-ex-icon"]}
+                      />
+
+                      <div className={styles["selected-workout-history-ex-name"]}>
+                        {ex.exerciseName}
+                      </div>
+                    </div>
+
+                    <div className={styles["selected-workout-history-sets-column"]}>
+                      {ex.sets.map((set, i) => {
+                        return (
+                          <div key={set.id} className={styles["selected-workout-history-set-pill"]}>
+                            <span className={styles["selected-workout-history-set-label"]}>
+                              Set {i + 1}:
+                            </span>
+                            <span className={styles["selected-workout-history-set-value"]}>
+                              {set.weight} × {set.reps}
+                            </span>
+                          </div>
+                        );
+                      })}
+                    </div>
                   </div>
                 )
               }
@@ -67,3 +88,4 @@ export default function WorkoutHistoryItem({ workoutHistory, filteredWorkoutHist
     })
   )
 }
+
